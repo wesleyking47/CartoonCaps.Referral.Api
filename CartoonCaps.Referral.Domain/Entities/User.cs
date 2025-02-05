3205791937398b60fba8a6f1846a377bf1ee1f6a
Wesley King <wesley.king@leadingedje.com>
2025-02-05 16:26:28 -0500

    removed more user classes

--- CartoonCaps.Referral.Domain/Entities/User.cs
+++
@@ -1,7 +0,0 @@
-namespace CartoonCaps.Referral.Domain.Entities;
-
-public class User(string id, string name)
-{
-    public string Id { get; set; } = id;
-    public string Name { get; set; } = name;
-}
